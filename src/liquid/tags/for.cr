wmoxam/liquid-crystal

commit d911a780d2fc4d7dc1202db6e4676bccbd426e11

Performance optimiztions (#11)
* Performance improvements

* Ignore profiler output

* Reduce benchmark template size to something more real world

* This really has no effect

* Update results

* Make won't detect source changes in lib, so compile everytime

src/liquid/tags/for.cr
@@ -81,10 +81,7 @@ module Liquid
 
       if context.registers.has_key?(:for)
         if (hash = context.registers[:for].raw).is_a?(Hash)
-          hash.each_key do |key|
-            value = hash[key]
-            for_hash[key] = value
-          end
+          for_hash = hash
         end
       end
 
@@ -116,43 +113,39 @@ module Liquid
       return render_else(context) if segment.empty?
 
       segment = segment.reverse if !@reversed.nil?
-      result = String::Builder.new
-      length = segment.size
+      String.build do |result|
+        length = segment.size
 
-      # Store our progress through the collection for the continue flag
-      for_hash[@name] = (from + segment.size).as(Type)
+        # Store our progress through the collection for the continue flag
+        for_hash[@name] = (from + segment.size).as(Type)
 
-      context.registers[:for] = {} of String => Type
-      for_hash.each_key do |key|
-        value = for_hash[key]
-        context.registers[:for][key] = value
-      end
+        context.registers[:for] = for_hash
 
-      context.stack do
-        segment.each_with_index do |item, index|
-          context[@variable_name] = item
-          context["forloop"] = Data.prepare({
-            "name"    => @name,
-            "length"  => length,
-            "index"   => index + 1,
-            "index0"  => index,
-            "rindex"  => length - index,
-            "rindex0" => length - index - 1,
-            "first"   => (index == 0),
-            "last"    => (index == length - 1),
-          })
+        context.stack do
+          segment.each_with_index do |item, index|
+            context[@variable_name] = item
+            context["forloop"] = Data.prepare({
+              "name"    => @name,
+              "length"  => length,
+              "index"   => index + 1,
+              "index0"  => index,
+              "rindex"  => length - index,
+              "rindex0" => length - index - 1,
+              "first"   => (index == 0),
+              "last"    => (index == length - 1),
+            })
 
-          result << render_all(@for_block, context)
+            result << render_all(@for_block, context)
 
-          # Handle any interrupts if they exist.
-          if context.has_interrupt?
-            interrupt = context.pop_interrupt
-            break if interrupt.is_a? BreakInterrupt
-            next if interrupt.is_a? ContinueInterrupt
+            # Handle any interrupts if they exist.
+            if context.has_interrupt?
+              interrupt = context.pop_interrupt
+              break if interrupt.is_a? BreakInterrupt
+              next if interrupt.is_a? ContinueInterrupt
+            end
           end
         end
       end
-      result.to_s
     end
 
     private def render_else(context)

.gitignore
@@ -7,3 +7,4 @@ lib
 examples/benchmark/benchmark
 examples/benchmark/lib
 examples/benchmark/.bundle/
+callgrind.out*

examples/benchmark/Makefile
@@ -1,13 +1,12 @@
 .PHONY: test
 
-all: compile test
+all: deps test
 
 test:
-	bundle install --path .bundle
 	bundle exec ruby --yjit src/benchmark.rb
+	crystal build --release src/benchmark.cr
 	./benchmark
 
-compile:
+deps:
 	shards install
-	crystal build --release src/benchmark.cr
-
+	bundle install --path .bundle

examples/benchmark/src/benchmark.cr
@@ -17,48 +17,7 @@ liquid_template = <<-END
     </div>
   </div>
 {% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
+
 
 END
 

examples/benchmark/src/benchmark.rb
@@ -16,48 +16,7 @@ liquid_template = <<-END
     </div>
   </div>
 {% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
-{% for product in products %}
-  <div class='product_brick'>
-    <div class='container'>
-      <div class='element'>
-        <img src='images/{{ product.image }}' class='product_miniature' />
-      </div>
-      <div class='element description'>
-        <a href={{ product.url }} class='product_name block bold'>
-          {{ product.external_index }}
-        </a>
-      </div>
-    </div>
-  </div>
-{% endfor %}
+
 
 END
 

src/liquid/block.cr
@@ -28,7 +28,7 @@ module Liquid
             end
           else
             raise SyntaxError.new "Tag '#{token}' was not properly terminated" \
-             " with regexp: #{TagEnd.inspect} "
+                                  " with regexp: #{TagEnd.inspect} "
           end
         when IsVariable
           @nodelist << create_variable(token)
@@ -54,7 +54,7 @@ module Liquid
         raise SyntaxError.new "#{block_name} tag does not expect else tag"
       when "end"
         raise SyntaxError.new "'end' is not a valid delimiter for " \
-          "#{block_name} tags. use #{block_delimiter}"
+                              "#{block_name} tags. use #{block_delimiter}"
       else
         raise SyntaxError.new "Unknown tag '#{tag}'"
       end
@@ -73,7 +73,7 @@ module Liquid
         return Variable.new(content[1])
       end
       raise SyntaxError.new("Variable '#{token}' was not properly terminated" \
-                           " with regexp: #{VariableEnd.inspect} ")
+                            " with regexp: #{VariableEnd.inspect} ")
     end
 
     def render(context)
@@ -112,6 +112,5 @@ module Liquid
         end
       end
     end
-
   end
 end

src/liquid/context.cr
@@ -23,6 +23,13 @@ module Liquid
     getter scopes : Array(Hash(String, Type)), :errors, :registers, :environments
     @literals : Hash(String, Type)
 
+    FLOATS   = /^(-?\d[\d\.]+)$/
+    INTEGERS = /^(-?\d+)$/
+    RANGES   = /^\((\S+)\.\.(\S+)\)$/
+    STRINGS  = /^['"](.*)['"]$/
+
+    LITERALS = Regex.union([FLOATS, INTEGERS, RANGES, STRINGS])
+
     def initialize(environments = [] of Hash(String, Type),
                    outer_scope = {} of String => Type,
                    registers = {} of Symbol => Type,
@@ -174,26 +181,30 @@ module Liquid
     private def resolve(key)
       if key.nil?
         return nil
-      elsif key.is_a?(String) && @literals.has_key?(key)
-        @literals[key]
-      else
-        is_variable = false
-        value = case key
-                when /^'(.*)'$/ # Single quoted strings
-                  $1
-                when /^"(.*)"$/ # Double quoted strings
-                  $1
-                when /^(-?\d+)$/ # Integer and floats
-                  $1.to_i
-                when /^\((\S+)\.\.(\S+)\)$/ # Ranges
-                  range_start = resolve($1).as Int32 | String
-                  range_end = resolve($2).as Int32 | String
-                  (range_start.to_i..range_end.to_i)
-                when /^(-?\d[\d\.]+)$/ # Floats
-                  $1.to_f
-                else
-                  is_variable = true
+      end
+
+      if key.is_a?(String)
+        if @literals.has_key?(key)
+          return @literals[key]
+        end
+
+        is_variable = !LITERALS.match(key)
+
+        value = if is_variable
                   variable(key)
+                else
+                  case key
+                  when STRINGS
+                    $1
+                  when INTEGERS
+                    $1.to_i
+                  when RANGES
+                    range_start = resolve($1).as Int32 | String
+                    range_end = resolve($2).as Int32 | String
+                    (range_start.to_i..range_end.to_i)
+                  when FLOATS
+                    $1.to_f
+                  end
                 end
 
         if !is_variable && key.is_a?(String)
@@ -201,6 +212,8 @@ module Liquid
         end
 
         value
+      else
+        variable(key)
       end
     end
 

commit 697c9f87fbc38cdde65fb2afdf094c4acca6f1a4

Fix for loop regression from #11

src/liquid/tags/for.cr
@@ -79,12 +79,20 @@ module Liquid
         rindex - 1
       end
 
-      def first?
+      def first
         @index == 0
       end
 
-      def last?
+      def first?
+        first
+      end
+
+      def last
         @index == (length - 1)
+      end
+
+      def last?
+        last
       end
     end
 