c2c19d2a29b5894acae5e78f1af958b9791334e0
wmoxam <me@wmoxam.com>
2023-10-23 08:50:21 -0400

    Don't re-compile regular expressions every time the code is ran. Huge performance gains!

--- src/liquid/variable.cr
+++ src/liquid/variable.cr
@@ -19,25 +19,31 @@ module Liquid
   end
 
   class Variable
-    FilterParser = /(?:#{FilterSeparator}|(?:\s*(?:#{QuotedFragment}|#{ArgumentSeparator})\s*)+)/
+    SeparateFiltersParser = /#{FilterSeparator}\s*(.*)/
+    FilterArgumentsParser = /(?:\A|#{ArgumentSeparator})\s*((?:\w+\s*\:\s*)?#{QuotedFragment})/
+    FilterParser          = /(?:#{FilterSeparator}|(?:\s*(?:#{QuotedFragment}|#{ArgumentSeparator})\s*)+)/
+    FilterNameParser      = /\s*(\w+)(?:\s*#{FilterArgumentSeparator}(.*))?/
+    TagAttributesParser   = /\A#{TagAttributes}\z/
+    QuotedFragmentParser  = /\s*(#{QuotedFragment})(.*)/
+
     getter :filters, :name
 
     def initialize(markup : String)
       @markup = markup
       @name = uninitialized String
       @filters = [] of FilterParams
-      if name_match = markup.match(/\s*(#{QuotedFragment})(.*)/)
+      if name_match = markup.match(QuotedFragmentParser)
         @name = name_match[1]
-        if filter_match = name_match[2].match(/#{FilterSeparator}\s*(.*)/)
+        if filter_match = name_match[2].match(SeparateFiltersParser)
           filters = filter_match[1].scan(FilterParser)
           filters.each do |f|
-            if f && (matches = f[0].match(/\s*(\w+)(?:\s*#{FilterArgumentSeparator}(.*))?/))
+            if f && (matches = f[0].match(FilterNameParser))
               filtername = matches[1]
               filterargs = if matches[2]?.nil?
-                [] of String
-              else
-                matches[2].to_s.scan(/(?:\A|#{ArgumentSeparator})\s*((?:\w+\s*\:\s*)?#{QuotedFragment})/).flatten.map { |m| m[1]?.to_s.strip }
-              end
+                             [] of String
+                           else
+                             matches[2].to_s.scan(FilterArgumentsParser).flatten.map { |m| m[1]?.to_s.strip }
+                           end
 
               @filters << FilterParams.new(filtername, filterargs)
             end
@@ -56,7 +62,7 @@ module Liquid
         filterargs = [] of Type
         keyword_args = {} of String => Type
         filter.filterargs.each do |a|
-          if matches = a.match(/\A#{TagAttributes}\z/)
+          if matches = a.match(TagAttributesParser)
             keyword_args[matches[1]] = context[matches[2]]
           else
             filterargs << context[a]
@@ -70,25 +76,25 @@ module Liquid
             context.invoke(filter.filtername, output)
           when 1
             context.invoke(filter.filtername, output,
-                           *Tuple(Type).from(filterargs))
+              *Tuple(Type).from(filterargs))
           when 2
             context.invoke(filter.filtername, output,
-                           *Tuple(Type, Type).from(filterargs))
+              *Tuple(Type, Type).from(filterargs))
           when 3
             context.invoke(filter.filtername, output,
-                           *Tuple(Type, Type, Type).from(filterargs))
+              *Tuple(Type, Type, Type).from(filterargs))
           when 4
             context.invoke(filter.filtername, output,
-                           *Tuple(Type, Type, Type, Type).from(filterargs))
+              *Tuple(Type, Type, Type, Type).from(filterargs))
           when 5
             context.invoke(filter.filtername, output,
-                           *Tuple(Type, Type, Type, Type, Type).from(filterargs))
+              *Tuple(Type, Type, Type, Type, Type).from(filterargs))
           else
             raise "Currently no support for more than 5 arguments, sorry!"
           end
         rescue FilterNotFound
           raise FilterNotFound.new "Error - filter '#{filter.filtername}' in" \
-           " '#{@markup.strip}' could not be found."
+                                   " '#{@markup.strip}' could not be found."
         end
       end
     end
