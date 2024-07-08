9432eb6193f47d502b09ab87a8539f2de8476702
“Wesley <wes@crowdmark.com>
2024-07-08 10:08:15 -0400

    Fixes enumerable drop test

--- test/liquid/tags/html_tag_test.cr
+++ test/liquid/tags/html_tag_test.cr
@@ -3,19 +3,21 @@ require "../../test_helper"
 class HtmlTagTest < Minitest::Test
   include Liquid
 
-  # class ArrayDrop < Liquid::Drop
-  #   include Enumerable(Type)
-  #
-  #   @array : Array(Type)
-  #
-  #   def initialize(array)
-  #     @array = array
-  #   end
-  #
-  #   # def each(&block)
-  #   #   @array.each(&block)
-  #   # end
-  # end
+  class ArrayDrop < Liquid::Drop
+    include Enumerable(Type)
+
+    @array : Array(Type)
+
+    def initialize(array)
+      @array = array.map { |i| i.as(Type) }
+    end
+
+    def each(&)
+      @array.each do |item|
+        yield item
+      end
+    end
+  end
 
   def test_html_table
     result = <<-RESULT
@@ -57,11 +59,11 @@ class HtmlTagTest < Minitest::Test
       {"collections" => {"frontpage" => [1, 2, 3, 4, 5, 6]}})
   end
 
-  # def test_enumerable_drop
-  #   assert_template_result("<tr class=\"row1\">\n<td class=\"col1\"> 1 </td><td class=\"col2\"> 2 </td><td class=\"col3\"> 3 </td></tr>\n<tr class=\"row2\"><td class=\"col1\"> 4 </td><td class=\"col2\"> 5 </td><td class=\"col3\"> 6 </td></tr>\n",
-  #                          "{% tablerow n in numbers cols:3%} {{n}} {% endtablerow %}",
-  #                          {"numbers" => ArrayDrop.new([1,2,3,4,5,6])})
-  # end
+  def test_enumerable_drop
+    assert_template_result("<tr class=\"row1\">\n<td class=\"col1\"> 1 </td><td class=\"col2\"> 2 </td><td class=\"col3\"> 3 </td></tr>\n<tr class=\"row2\"><td class=\"col1\"> 4 </td><td class=\"col2\"> 5 </td><td class=\"col3\"> 6 </td></tr>\n",
+      "{% tablerow n in numbers cols:3%} {{n}} {% endtablerow %}",
+      {"numbers" => ArrayDrop.new([1, 2, 3, 4, 5, 6])})
+  end
 
   def test_offset_and_limit
     assert_template_result("<tr class=\"row1\">\n<td class=\"col1\"> 1 </td><td class=\"col2\"> 2 </td><td class=\"col3\"> 3 </td></tr>\n<tr class=\"row2\"><td class=\"col1\"> 4 </td><td class=\"col2\"> 5 </td><td class=\"col3\"> 6 </td></tr>\n",
